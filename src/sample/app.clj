63e32b5f2e3b6268aa3b9da9e6afca9a88451e05
Denis Cornehl <denis.cornehl@gmail.com>
2013-11-20 17:31:05 +0100

    prettier clojure - testpage

--- src/sample/app.clj
+++ src/sample/app.clj
@@ -1,13 +1,33 @@
 (ns sample.app
-  (:require [ring.adapter.jetty :as jetty]
-            [compojure.core :refer [defroutes GET]]))
+  (:use compojure.core)  
+  (:use ring.middleware.resource)  
+  (:require 
+          [ring.adapter.jetty :as jetty]
+          [compojure.route :as route]
+          [compojure.handler :as handler]))
 
-(defroutes handler
-  (GET "/" []
-       {:headers {"Content-type" "text/plain; charset=UTF-8"}
-        :body "Hello world!"}))
+(defroutes main-routes    
+  (GET "/" [] "<!doctype html>
+<html>
+<head>
+	<link href='helloworld.css' rel='stylesheet' type='text/css'/>
+	<title>Hello World in Clojure</title>
+</head>
+<body>
+	<strong>Hello world, I'm a Clojure app running on cloudControl!</strong>
+  <footer>
+    Coming from the <a href='https://www.cloudcontrol.com/dev-center/Quickstart'>Quickstart</a>?
+    Check out <a href='https://www.cloudcontrol.com/dev-center/Guides/Java/Clojure'>the explanation</a>.
+  </footer>
+</body>
+</html>")
+  (route/resources "/")
+  (route/not-found "<h1>Page not found</h1>"))
+
+(def app  
+  (handler/site main-routes))
 
 (defn -main []
-  (jetty/run-jetty handler
+  (jetty/run-jetty main-routes
                    {:port (Integer. (or (System/getenv "PORT") "8080"))
                     :join? false}))
